74a999253e67c3d2b8bfc76b53b9b08150bc5094
brunosvara <brunosvara032@gmail.com>
2023-04-16 23:58:11 +0200

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,126 @@
+import { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);   
+
+    emailjs.send(
+      'service_f5eospr', 
+      'template_9hbs0me',
+      {
+        from_name: form.name,
+        to_name: 'Francesco',
+        from_email: form.email,
+        to_email: 'francesco.svara@gmail.com',
+        message: form.message,
+      },
+      'T0eewF6hqySzYujwQ'
+      )
+      .then(() => {
+        setLoading(false);
+        alert('Grazie. Sarai ricontattato al più presto');
+
+        setForm({
+          name: '',
+          email: '',
+          message: '',
+        })
+      }, (error) => {
+        setLoading(false)
+
+        console.log(error);
+
+        alert('Il tuo messaggio non è stato inviato.')
+      })
+  };
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn('left', "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Contattaci</p>
+        <h3 className={styles.sectionHeadText}>Contatti</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+          >
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Il tuo nome</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                placeholder="Come ti chiami?"
+                className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+              />
+            </label>
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">La tua Email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder="Qual è la tua email?"
+                className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+              />
+            </label>
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Il tuo messaggio</span>
+              <textarea
+                rows="7"
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                placeholder="Cosa ti interessa sapere?"
+                className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium resize-none"
+              />
+            </label>
+
+            <buttom
+              type="submit"
+              className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+            >
+              {loading ? 'Sending...' : 'Invia'}
+            </buttom>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn('right', "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  )
+}
+
+export default SectionWrapper(Contact, "contatti")
---
+++ src/constants/index.js
@@ -0,0 +1,268 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "progetti",
+      title: "Progetti",
+    },
+    {
+      id: "contatti",
+      title: "Contatti",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Frontend Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Data Analyst",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Self-taughting",
+      //company_name: "Starbucks",
+      icon: web,
+      iconBg: "#383E56",
+      date: "Marzo 2016 - Aprile 2019",
+      points: [
+        "Inizio e formazione Self-taughting con particolare riferimento ai linguaggi html e css.",
+        "Centinaia di ore di tutorial visionati con implementazione delle conoscenze di WordPress, Canva, Javascript.",
+        "In veste di Frontend Developer inizio collaborazione con altri sviluppatori per creare prodotti da inserire online.",
+        "Studio e formazione di librerie javascript con particolare attenzione a  React, Three.js e JQuery.",
+      ],
+    },
+    {
+      title: "Full Stack Developer",
+      //company_name: "Tesla",
+      icon: mobile,
+      iconBg: "#E6DEDD",
+      date: "Maggio 2019 - Dicembre 2022",
+      points: [
+        "Sviluppo e manutenzione di applicazioni Web utilizzando WordPress e altre tecnologie correlate.",
+        "Collaborazione con team interfunzionali tra cui designer, product manager e altri sviluppatori per creare prodotti di alta qualità.",
+        "Implementazione del responsive design e garanzia della compatibilità cross-browser.",
+        "Partecipazione alle revisioni del codice e rilascio feedback costruttivi ad altri sviluppatori",
+      ],
+    },
+    {
+      title: "Certificazioni e Attestati",
+      //company_name: "Shopify",
+      icon: backend,
+      iconBg: "#383E56",
+      date: "Gennaio 2023 - Marzo 2023",
+      points: [
+        "Corso di formazione Aulab Hackademy - Full immersion di 400 ore - Gennaio/Marzo 2023.",
+        "Competenze e conoscenze certificate: html, css, javascript, php, framework laravel e metodologie agili.",
+        "Meetup Coding Zone «Filament PHP: lo strumento lowcode che sfrutta lo stack TALL».",
+        "Attestato Boolean Data Week Plus - Analisi dei dati statistici · Python · Analisi dei dati · google sheets",
+      ],
+    },
+    {
+      title: "Work in Progress",
+      //company_name: "Meta",
+      icon: creator,
+      iconBg: "#E6DEDD",
+      date: "Apr 2023 - ...",
+      points: [
+        "Angular 2+ - framework open source per lo sviluppo di applicazioni web con licenza MIT, evoluzione di AngularJS.",
+        "Vue.js - framework JavaScript open-source in configurazione Model–view–viewmodel per la creazione di interfacce utente e single-page applications.",
+        "Blender - software libero e multipiattaforma di modellazione, rigging, rendering e texturing di immagini tridimensionali e bidimensionali.",
+        "Magento - CMS open source per l'e-commerce lanciato il 31 marzo 2008. Creato dalla Varien e costruito sopra Zend Framework.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Pensavo fosse impossibile realizzare un sito web bello come il nostro prodotto, ma Francesco mi ha smentito.",
+      name: "Silvia Torchio",
+      designation: "CEO",
+      company: "Mary Host",
+      image: "src/assets/company/torchio.png"
+      //image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "Non ho mai incontrato uno sviluppatore web che si preoccupi veramente del successo dei propri clienti come fa Francesco.",
+      name: "Vincenzo Pandolfi",
+      designation: "CEO",
+      company: "Essebi Srl",
+      image: "src/assets/company/pandolfi.png"
+      //image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "Dopo che Francesco ha ottimizzato il nostro sito web, il traffico è aumentato del 50%. Non possiamo ringraziarlo abbastanza!",
+      name: "Carla Terlizzi",
+      designation: "CEO",
+      company: "Foods Delivery App",
+      image: "src/assets/company/terlizzi.png"
+      //image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Web Agency",
+      description:
+        "Sito Web dell'azienda Pixelpro. Qui puoi visionare i nostri progetti, consultare il listino prezzi e i tempi di esecuzione dei lavori. Home page con animazione 3D.",
+      tags: [
+        {
+          name: "threejs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "css",
+          color: "green-text-gradient",
+        },
+        {
+          name: "javascript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Property Manager",
+      description:
+        "Sito di professionista che gestisce immobili per conto dei proprietari allo scopo di tutelare ed accrescere il valore del patrimonio immobiliare",
+      tags: [
+        {
+          name: "html",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "css",
+          color: "green-text-gradient",
+        },
+        {
+          name: "javascript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Real Estate",
+      description:
+        "Agenzia immobiliare a servizio completo che offre una vasta gamma di servizi per aiutare i clienti a comprare, vendere e gestire le loro proprietà.",
+      tags: [
+        {
+          name: "laravel",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "php",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
